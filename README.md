7b3d810364b5bf0da191abc511242ca82a29a034
Lee Walling <wallinglee@yahoo.com>
2024-02-06 10:25:27 -0500

    Initial repo commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Star Rating component
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Standard star rating component built with React.
 
 ## Available Scripts
 
---
+++ src/components/star-rating/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { FaStar } from "react-icons/fa";
+import './styles.css';
+
+const StarRating = ({ numberOfStars = 10 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleMouseEnter(currentIndex) {
+    setHover(currentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
+  function handleClick(currentIndex) {
+    setRating(currentIndex);
+  }
+
+  return (
+    <div className="star-rating">
+      <div>Current rating: {rating}</div>
+
+      {[...Array(numberOfStars)].map((star, index) => {
+        index += 1;
+        return (
+          <FaStar
+            size={50}
+            className={"star " + (index <= (hover || rating) ? "active" : "inactive")}
+            title={`Click this to set the rating to ${index}`}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            onClick={() => handleClick(index)}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default StarRating
